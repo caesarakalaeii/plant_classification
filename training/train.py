f1c31c635c733c250d1a4b535dc9e3aa7c53d4df
kwittke <kilian.wittke@stud-hs-bochum.de>
2024-12-08 16:13:31 +0100

    Prototype training script.

---
+++ training/train.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("model/yolo11n.pt")  # load a pretrained model (recommended for training)
+# Training kann ganz normal erfolgen, wenn man die alten Klassen nicht braucht. Sonst sollte man das Datenset ergänzen.
+
+# Train the model with MPS
+results = model.train(data="plants.yaml",
+                      epochs=300,
+                      imgsz=640,
+                      patience=5,
+                      dropout=0.2,
+                      plots=True,
+                      degrees=90,
+                      flipud=0.5,
+                      fliplr=0.5,
+                      mosaic=1,
+                      erasing=0.4,
+                      crop_fraction=1)
