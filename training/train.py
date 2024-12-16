c9df8d47aacf89cb96930b92f02374b583378a50
kwittke <kilian.wittke@stud-hs-bochum.de>
2024-12-16 17:27:13 +0100

    Added yaml trainingsset

--- training/train.py
+++ training/train.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("model/yolo11n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("model/yolo11x.pt")  # load a pretrained model (recommended for training)
 # Training kann ganz normal erfolgen, wenn man die alten Klassen nicht braucht. Sonst sollte man das Datenset ergänzen.
 
 # Train the model with MPS
-results = model.train(data="plants.yaml",
+results = model.train(data="data/plant classification/data.yaml",
                       epochs=300,
                       imgsz=640,
                       patience=5,
