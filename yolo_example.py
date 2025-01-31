38b315b8135270b765c094e2466c345ddd7f4065
Distiny42 <reithnils@gmail.com>
2025-01-31 20:34:14 +0100

    Added example_GUI

--- yolo_example.py
+++ yolo_example.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("training/model/best.pt")
+model = YOLO("training/Models/best_train13.pt")
 
-result = model("training/data/plant classification/test/images/Colchicum_autumnale_L-_4_14_jpg.rf.6b0d6a3d457d4a75d84c04af77caf1cd.jpg")
+result = model("training/data/plant classification/test/images/Digitalis_L-_4_6_jpg.rf.6648ff491b14e75efb64881da024062d.jpg")
 
 for bb in result:
     print(bb.boxes.cls[0])
     print(bb.boxes.xywh[0])
+    print(bb.boxes.conf[0])
